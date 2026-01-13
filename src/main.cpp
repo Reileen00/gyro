71cf2c5d20ae38b91c4e551e92f4d21ab79863e2
Reileen00 <100047799+Reileen00@users.noreply.github.com>
2026-01-13 09:04:21 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,3 +8,9 @@ z=gyro.mobius_add(x,y)
 v=gyro.expmap(x,y)
 u=gyro.logmap(x,z)
 d=gyro.distance(x,y)
+
+
+A = np.random.randn(100000,3)*0.1
+B = np.random.randn(100000,3)*0.1
+
+C = gyro.mobius_add_cuda(A,B,1.0)
